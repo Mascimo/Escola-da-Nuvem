1c3ca110b07659968a2e2aedaff12e569e177696
Pedro Mascimo <pedromascimo@hotmail.com>
2026-01-08 20:47:08 -0300

    Atividade

---
+++ atividade-pratica01/questao03.py
@@ -0,0 +1,8 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print("O volume do retângulo é:", volume,"cm³" )
+
---
+++ atividade-pratica01/questao04.py
@@ -0,0 +1,11 @@
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+valor_final = preco_unitario * quantidade
+
+print("TOTAL COMPRADO")
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: {preco_unitario:.2f}")
+print(f"Quantidade Comprada: {quantidade}")
+print(f"Total: {valor_final:.2f}")
